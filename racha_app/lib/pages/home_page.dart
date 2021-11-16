e37a1cbe0b3824f68df9c9e38d3c4b658cbf2890
emerson <emersonvieiramartins@hotmail.com>
2021-11-16 15:22:42 -0300

    teste

--- racha_app/lib/pages/home_page.dart
+++ racha_app/lib/pages/home_page.dart
@@ -30,7 +30,7 @@ class BNBCustomPainter extends CustomPainter {
     path.quadraticBezierTo(size.width * 0.20, 0, size.width * 0.35, 0);
     path.quadraticBezierTo(size.width * 0.40, 0, size.width * 0.40, 20);
     path.arcToPoint(Offset(size.width * 0.60, 20),
-        radius: Radius.circular(20.0), clockwise: false);
+        radius: Radius.circular(10.0), clockwise: false);
     path.quadraticBezierTo(size.width * 0.60, 0, size.width * 0.65, 0);
     path.quadraticBezierTo(size.width * 0.80, 0, size.width, 0); //end
     path.lineTo(size.width, size.height);
@@ -55,7 +55,10 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
-//------------------------------------------------------------------------------
+//-------------------------------
+//
+//
+//-----------------------------------------------
   @override
   Widget _body() {
     // NAVIGATION BAR
